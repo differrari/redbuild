710631073ca1259613500ac75df2b44877c19136
di <git@diferrari.com>
2026-01-19 00:00:00 +0000

    [FS] moved buffer to redlib

--- redbuild.c
+++ redbuild.c
@@ -271,45 +271,6 @@ void add_linker_flag(char *name, bool back){
     push_lit(back ? link_flags_list_b : link_flags_list_f, name);
 }
 
-typedef struct {
-    char* buffer;
-    size_t buffer_size;
-    size_t limit;
-    bool can_grow;
-    bool circular;
-    uintptr_t cursor;
-} buffer;
-
-buffer buffer_create(size_t size, bool can_grow, bool circular){
-    return (buffer){
-        .buffer = malloc(size),
-        .buffer_size = 0,
-        .limit = size,
-        .can_grow = can_grow,
-        .circular = circular,
-        .cursor = 0,
-    };
-}
-
-void buffer_write(buffer *buf, char* fmt, ...){
-    __attribute__((aligned(16))) va_list args;
-    va_start(args, fmt); 
-    size_t n = string_format_va_buf(fmt, buf->buffer+buf->cursor, buf->limit-buf->cursor, args);
-    buf->cursor += n;
-    buf->buffer_size += n;
-    if (buf->can_grow && buf->buffer_size > buf->limit-256){
-        size_t new_size = buf->limit;
-        buf->buffer = realloc_sized(buf->buffer, buf->limit, new_size);
-        buf->limit = new_size;
-    }
-    //TODO: circular
-    va_end(args);
-}
-
-void buffer_write_space(buffer *buf){
-    buffer_write(buf, " ");
-}
-
 buffer buf;
 
 void list_strings(void *data){
