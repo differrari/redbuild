98b53b38c6f58238a4977c14ddd59537a86e8fba
di <git@diferrari.com>
2026-07-19 00:00:00 +0000

    [PROJ] remove gitignored playground

--- playground/build.lisp
+++
@@ -1,8 +0,0 @@
-(load "../v3/redbuild.lisp")
-
-(redbuild:quick-build (redbuild:make-instance `redbuild:redmod
-        :name "play"
-        :type :lib
-        :target (redbuild:native)
-        :srcs (list "test.c" "a.c")
-) :add-dependencies t :run t :success (lambda () (print "Done") (print (redbuild:emit-compile-commands))))
--- playground/test.c
+++
@@ -1,7 +0,0 @@
-#include "memory/memory.h"
-#include "syscalls/syscalls.h"
-
-int main(){
-    print("Hello world");
-    // memcpy(0,0,0);
-}
